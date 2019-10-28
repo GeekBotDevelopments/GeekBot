4bf108e148a919bc7dba3c7e49b6164291a76903
LegendaryGeek <timothydaley4@gmail.com>
2019-10-28 08:56:53 -0400

    added an event for minecraft, doesnt work at all yet

---
+++ src/main/java/bot/events/MinecraftEvent.java
@@ -0,0 +1,17 @@
+package bot.events;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.event.domain.Event;
+
+public class MinecraftEvent extends Event {
+
+	protected MinecraftEvent(DiscordClient client) {
+		super(client);
+
+	}
+
+	public void MinecraftEvent() {
+
+	}
+
+}
