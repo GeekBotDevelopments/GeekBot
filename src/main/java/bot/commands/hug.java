7f1d3484d136e0af52129be23518da508abe7de6
LegendaryGeek <timothydaley4@gmail.com>
2019-10-29 08:48:39 -0400

    still havent gotten hugging other people working yet

--- src/main/java/bot/commands/hug.java
+++ src/main/java/bot/commands/hug.java
@@ -1,21 +1,22 @@
 package bot.commands;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.util.Snowflake;
 
 public class hug {
 
 	public static String hug(MessageCreateEvent event) {
 		String mention = event.getMember().get().getMention();
-//		Snowflake memberId = null;
-		String messageToSend = mention + " *hug*";
+		Snowflake id = event.getMember().get().getId();
+		String messageToSend = mention + " *hugs*";
 		String messageSent = event.getMessage().getContent().get();
 		System.out.println("[hug] message sent: " + messageSent);
-//		if(messageSent.startsWith("!gb hug ")) {
-//			
-//			
-//		}
-//		
+		if (messageSent.equals("hug %s")) {
+			String message = String.format(messageSent, id);
+			Member member = event.getGuild().block().getMemberById(id).block();
+			messageToSend = member.getMention() + " *hugs*";
+		}
 		return messageToSend;
 	}
 
