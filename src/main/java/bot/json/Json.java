b7e86ba32482c0dda90d486526dbf6afbd9d1587
LegendaryGeek <timothydaley4@gmail.com>
2019-10-29 08:49:13 -0400

    start of a json utilities file

---
+++ src/main/java/bot/json/Json.java
@@ -0,0 +1,14 @@
+package bot.json;
+
+import com.google.gson.Gson;
+
+import discord4j.core.object.entity.Guild;
+
+public class Json {
+	Gson gson = new Gson();
+	String databasePath = "C:\\programfiles\\geekbot\\";
+	public void ServerSettings(Guild guild) {
+		 
+	}
+
+}
