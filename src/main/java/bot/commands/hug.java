b5662521c93ba532cb50769f8dfdeddd0bf5ff89
LegendaryGeek <timothydaley4@gmail.com>
2019-11-02 16:37:20 -0400

    fixed hug

--- src/main/java/bot/commands/hug.java
+++ src/main/java/bot/commands/hug.java
@@ -1,21 +1,30 @@
 package bot.commands;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Member;
 import discord4j.core.object.util.Snowflake;
 
 public class hug {
+	private static Logger log = LogManager.getLogger(hug.class);
 
 	public static String hug(MessageCreateEvent event) {
 		String mention = event.getMember().get().getMention();
-		Snowflake id = event.getMember().get().getId();
 		String messageToSend = mention + " *hugs*";
 		String messageSent = event.getMessage().getContent().get();
-		System.out.println("[hug] message sent: " + messageSent);
-		if (messageSent.equals("hug %s")) {
-			String message = String.format(messageSent, id);
-			Member member = event.getGuild().block().getMemberById(id).block();
-			messageToSend = member.getMention() + " *hugs*";
+		try {
+			if (messageSent.length() >= 8) {
+				String userid = messageSent.substring(8);
+				Snowflake flake = Snowflake.of(userid);
+				Member member = event.getGuild().block().getMemberById(flake).block();
+				messageToSend = member.getMention() + " *hugs*";
+			}
+		} catch (Exception e) {
+			log.catching(Level.ERROR, e);
+			messageToSend = mention + "incorrect arguments, only member id's are supported not pings";
 		}
 		return messageToSend;
 	}
