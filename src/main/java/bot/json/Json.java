0e378f93976c3e0afa6cbf5523273ee39b0e54f6
LegendaryGeek <timothydaley4@gmail.com>
2019-11-02 16:33:43 -0400

    moved some stuff to here

--- src/main/java/bot/json/Json.java
+++ src/main/java/bot/json/Json.java
@@ -1,14 +1,99 @@
 package bot.json;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import discord4j.core.object.entity.Guild;
 
 public class Json {
+	static Logger log = LogManager.getLogger(Json.class);
 	Gson gson = new Gson();
 	String databasePath = "C:\\programfiles\\geekbot\\";
+
 	public void ServerSettings(Guild guild) {
-		 
+
+	}
+
+	public static JsonElement getJsonFileFromWeb(String url) throws IOException {
+		Gson gson = new Gson();
+
+		// URL declaration
+		URL obj = new URL(url);
+
+		// URL connection
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// Request Settings
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+		// check response code for an okay
+		int responseCode = con.getResponseCode();
+		log.info("GET Response Code: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			// Read the Response from the site
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			JsonParser parsely = new JsonParser();
+			JsonElement jobj = parsely.parse(in);
+
+			// Generate a response to return
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			// Close Reader
+			in.close();
+
+			// print result
+			return jobj;
+		}
+		log.error("GET Failed");
+		return null;
+	}
+
+	public static String get(String url) throws IOException {
+		// URL declaration
+		URL obj = new URL(url);
+
+		// URL connection
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// Request Settings
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+		// check response code for an okay
+		int responseCode = con.getResponseCode();
+		log.info("GET Response Code: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			// Read the Response from the site
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			// Generate a response to return
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			// Close Reader
+			in.close();
+
+			// print result
+			return response.toString();
+		}
+		return null;
 	}
 
 }
