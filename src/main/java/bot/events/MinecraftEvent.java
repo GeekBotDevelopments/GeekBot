ee316ea99dccaba0509fbbe8cf5d6b4a85bb86c3
LegendaryGeek <timothydaley4@gmail.com>
2019-11-04 18:01:28 -0500

    changed extension

--- src/main/java/bot/events/MinecraftEvent.java
+++ src/main/java/bot/events/MinecraftEvent.java
@@ -3,7 +3,6 @@ package bot.events;
 import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.Event;
 
-public class MinecraftEvent extends Event {
 
 	protected MinecraftEvent(DiscordClient client) {
 		super(client);
