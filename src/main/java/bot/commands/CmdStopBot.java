37e89c35c88ed364b95134a891620a886773b112
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2020-09-25 14:40:25 -0400

    command to stop the bot

--- src/main/java/bot/commands/CmdStopBot.java
+++ src/main/java/bot/commands/CmdStopBot.java
@@ -2,6 +2,7 @@ package bot.commands;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.util.log.Log;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
@@ -22,6 +23,8 @@ public class CmdStopBot extends Command {
 		GeekBot.getClient().cancelRequests();
 		GeekBot.getClient().shutdown();
 		log.info("Shutdown");
+		System.exit(0);
+		log.info("Should not see thisss");
 	}
 
 }
