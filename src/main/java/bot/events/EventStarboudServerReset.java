07aeea81d181ed0845356bafcf10bf6201f042a6
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2020-09-25 14:41:21 -0400

    starbound sucessfully kills, but doesnt start

--- src/main/java/bot/events/EventStarboudServerReset.java
+++ src/main/java/bot/events/EventStarboudServerReset.java
@@ -1,6 +1,5 @@
 package bot.events;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.TimerTask;
@@ -13,7 +12,6 @@ import net.dv8tion.jda.api.entities.Role;
 
 public class EventStarboudServerReset extends TimerTask {
 	private static Logger log = LogManager.getLogger();
-	
 
 	public EventStarboudServerReset() {
 		// TODO Auto-generated constructor stub
@@ -23,12 +21,14 @@ public class EventStarboudServerReset extends TimerTask {
 	public void run() {
 		this.starbound_reset();
 	}
-	
-	public void starbound_reset() {
+
+	public synchronized void starbound_reset() {
+
 		log.info("restarting Server");
 		Role starbound = GeekBot.getClient().getGuildById(GeekBot.getLABRINTH_ID()).getRoleById(755597397710733352l);
-		Path serverPath = Paths.get("E:\\Games\\Steam\\steamapps\\common\\Starbound Dedicated Server\\win64");
-		GeekBot.getClient().getTextChannelById(GeekBot.getLABUPDATE()).sendMessage(starbound.getAsMention() + " server restart").submit();
+		Path serverPath = Paths.get("E:\\Games\\Steam\\steamapps\\common\\Starbound_Dedicated_Server\\win64\\");
+		GeekBot.getClient().getTextChannelById(GeekBot.getLABUPDATE())
+				.sendMessage(starbound.getAsMention() + " server restart").submit();
 		try {
 			log.info("Waiting 5 minutes");
 			this.wait(300000);
@@ -36,12 +36,18 @@ public class EventStarboudServerReset extends TimerTask {
 			log.catching(e);
 		}
 		Runtime rs = Runtime.getRuntime();
+		//Process process;
+		//ProcessBuilder pb = new ProcessBuilder("start " + serverPath.toString() + "starbound_server.exe");
 		try {
 			log.info("killing task");
 			rs.exec("Taskkill /IM starbound_server.exe /F");
 			
 			log.info("Starting task");
-			rs.exec("start \"Starbound Server\" " + serverPath.toString() + " \\starbound_server.exe");
+			
+			log.info(GeekBot.get("steam://rungameid/533830"));
+			//process = pb.start();
+			//process = rs.exec(serverPath.toString() + "starbound_server.exe" , null, serverPath.toFile());
+			//EventStarboundLog starlog = new EventStarboundLog(process.getErrorStream());
 		} catch (Exception e) {
 			log.catching(e);
 		}
