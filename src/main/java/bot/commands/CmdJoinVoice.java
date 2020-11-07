e3cbe5bf699687385564eaf42a5dad0ce1966bac
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2020-11-07 00:07:53 -0500

    attempt to do some voice stuff

--- src/main/java/bot/commands/CmdJoinVoice.java
+++ src/main/java/bot/commands/CmdJoinVoice.java
@@ -1,19 +1,24 @@
 package bot.commands;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import com.google.cloud.texttospeech.v1.TextToSpeechClient;
+import com.google.cloud.texttospeech.v1beta1.AudioConfig;
+import com.google.cloud.texttospeech.v1beta1.AudioEncoding;
+import com.google.cloud.texttospeech.v1beta1.SsmlVoiceGender;
+import com.google.cloud.texttospeech.v1beta1.SynthesisInput;
+import com.google.cloud.texttospeech.v1beta1.Voice;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
-import bot.GeekBot;
-import net.dv8tion.jda.api.entities.ChannelType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.dv8tion.jda.api.audio.AudioSendHandler;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.managers.AudioManager;
 
 public class CmdJoinVoice extends Command {
 	private static Logger log = LogManager.getLogger();
-	
+
 	public CmdJoinVoice() {
 		name = "join";
 		hidden = true;
@@ -23,12 +28,17 @@ public class CmdJoinVoice extends Command {
 	protected void execute(CommandEvent event) {
 		AudioManager manager = event.getGuild().getAudioManager();
 
-					VoiceChannel chan = event.getGuild().getVoiceChannelById(359460350988517378l);
-					manager.openAudioConnection(chan);
-					log.info("connected to channel: {}" + chan.getName());
-			
+		VoiceChannel chan = event.getGuild().getVoiceChannelById(359460350988517378l);
+		manager.openAudioConnection(chan);
+		log.info("connected to channel: {}" + chan.getName());
+		//manager.setSendingHandler(new AudioSendHandler().);
 
+		//Voice voice = Voice.newBuilder().addLanguageCodes("en-US").setSsmlGender(SsmlVoiceGender.FEMALE)
+		//		.setName("GeekBot").build();
+		//SynthesisInput input = SynthesisInput.newBuilder().setText("Hello World!").build();
+		//AudioConfig audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.OGG_OPUS).build();
+
+		//TextToSpeechClient ttsClient = TextToSpeechClient.create().synthesizeSpeech(input, voice, audioConfig);
 	}
-
 
 }
