4355d529bd3368a7d9b97596835e1f234da9c848
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2021-01-15 00:06:15 -0500

    delete un-needed files for a personal porject

--- src/main/java/bot/c_Conversion/AllMemory.java
+++
@@ -1,5 +0,0 @@
-package bot.c_Conversion;
-
-public class AllMemory extends Memory {
-    byte[] all;
-}
--- src/main/java/bot/c_Conversion/Registers.java
+++
@@ -1,9 +0,0 @@
-package bot.c_Conversion;
-
-public class Registers {
-    byte[]  a;
-    byte[]  x;
-    byte[]  y;
-    byte[]  z;
-    byte[] sp;
-}
