bdc66668050b78e58ccbef9dd65ff97194ed78f5
Robin Seifert <rseifert.phone@gmail.com>
2021-01-17 13:42:57 -0500

    Formatting welcome event

--- src/main/java/bot/events/WelcomeEvent.java
+++ src/main/java/bot/events/WelcomeEvent.java
@@ -7,31 +7,31 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberLeaveEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class WelcomeEvent extends ListenerAdapter {
+public class WelcomeEvent extends ListenerAdapter
+{
+    public WelcomeEvent()
+    {
+        // TODO Auto-generated constructor stub
+    }
 
-	public WelcomeEvent() {
-		// TODO Auto-generated constructor stub
-	}
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event)
+    {
+        // TODO Auto-generated method stub
+        super.onGuildMemberJoin(event);
+        TextChannel chan = event.getGuild().getSystemChannel();
+        Member member = event.getMember();
 
-	@Override
-	public void onGuildMemberJoin(GuildMemberJoinEvent event) {
-		// TODO Auto-generated method stub
-		super.onGuildMemberJoin(event);
-		TextChannel chan = event.getGuild().getSystemChannel();
-		Member member = event.getMember();
-		
-		chan.sendMessage("Welcome **" + member.getEffectiveName() + "** to " + event.getGuild().getName()).submit();
-	}
+        chan.sendMessage("Welcome **" + member.getEffectiveName() + "** to " + event.getGuild().getName()).submit();
+    }
 
-	@Override
-	public void onGuildMemberRemove(GuildMemberRemoveEvent event) {
-		super.onGuildMemberRemove(event);
-		TextChannel chan = event.getGuild().getSystemChannel();
-		Member member = event.getMember();
-		
-		chan.sendMessage("member " + member.getEffectiveName() + " has left").submit();
-	}
-	
-	
+    @Override
+    public void onGuildMemberRemove(GuildMemberRemoveEvent event)
+    {
+        super.onGuildMemberRemove(event);
+        TextChannel chan = event.getGuild().getSystemChannel();
+        Member member = event.getMember();
 
+        chan.sendMessage("member " + member.getEffectiveName() + " has left").submit();
+    }
 }
