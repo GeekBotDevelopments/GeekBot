0caa60e8e86a58d8115b27fba351331c6138ac66
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2021-03-13 21:01:41 -0500

    format and write getters and setters

--- src/main/java/bot/modules/minecraft/forge/ForgeVersion.java
+++ src/main/java/bot/modules/minecraft/forge/ForgeVersion.java
@@ -4,39 +4,45 @@ import bot.models.SemVer;
 import lombok.Data;
 
 @Data
-public class ForgeVersion implements Comparable<ForgeVersion>
-{
-    private final SemVer minecraft;
-    private final SemVer forge;
-    private final boolean latest;
+public class ForgeVersion implements Comparable<ForgeVersion> {
 
-    public ForgeVersion(String mcString, String forgeString)
-    {
-        final String[] split = mcString.split("-");
-        minecraft = new SemVer(split[0]);
-        latest = "latest".equalsIgnoreCase(split[1]);
-        forge = new SemVer(forgeString);
-    }
+  private final SemVer minecraft;
+  private final SemVer forge;
+  private final boolean latest;
 
-    @Override
-    public int compareTo(ForgeVersion other)
-    {
-        final int minecraftCompare = minecraft.compareTo(other.minecraft);
-        if (minecraftCompare != 0)
-        {
-            return minecraftCompare;
-        }
-        final int forgeCompare = forge.compareTo(other.forge);
-        if (forgeCompare != 0)
-        {
-            return forgeCompare;
-        }
-        return Boolean.compare(latest, other.latest);
-    }
+  public ForgeVersion(String mcString, String forgeString) {
+    final String[] split = mcString.split("-");
+    minecraft = new SemVer(split[0]);
+    latest = "latest".equalsIgnoreCase(split[1]);
+    forge = new SemVer(forgeString);
+  }
 
-    @Override
-    public String toString()
-    {
-        return String.format("%s-%s%s", minecraft, forge, (latest ? "L" : "R"));
+  @Override
+  public int compareTo(ForgeVersion other) {
+    final int minecraftCompare = minecraft.compareTo(other.minecraft);
+    if (minecraftCompare != 0) {
+      return minecraftCompare;
     }
+    final int forgeCompare = forge.compareTo(other.forge);
+    if (forgeCompare != 0) {
+      return forgeCompare;
+    }
+    return Boolean.compare(latest, other.latest);
+  }
+
+  public SemVer getMinecraft() {
+    return this.minecraft;
+  }
+
+  public SemVer getForge() {
+    return this.forge;
+  }
+
+  public boolean isLatest() {
+    return this.latest;
+  }
+
+  public boolean getLatest() {
+    return this.latest;
+  }
 }
