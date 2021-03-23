c89d7f190c6f7a1c1a6b72cb9339821269aa3d7f
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2021-03-23 14:52:32 -0400

    port the stop bot command

--- src/main/java/bot/modules/commands/CmdStopBot.java
+++ src/main/java/bot/modules/commands/CmdStopBot.java
@@ -1,7 +1,33 @@
 package bot.modules.commands;
 
-public class CmdStopBot //extends Command
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import bot.modules.discord.Command;
+import bot.modules.discord.DiscordModule;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+import reactor.core.publisher.Mono;
+
+public class CmdStopBot extends Command
 {
+    private static Logger log = LogManager.getLogger();
+
+    public CmdStopBot(String root) {
+        super("stop");
+    }
+
+    @Override
+    public boolean handle(Message message, MessageChannel channel, List<String> strings) {
+		log.info("Shutting down");
+		DiscordModule.client.logout();
+		log.info("Shutdown");
+		System.exit(0);
+		log.info("Should not see thisss");
+        return false;
+    }
 //	private static Logger log = LogManager.getLogger();
 //
 //	public CmdStopBot(){
