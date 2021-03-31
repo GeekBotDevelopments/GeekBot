c68c44ec06e1e91e8c8331aad40a6417f5002cf4
Robin Seifert <rseifert.phone@gmail.com>
2021-03-31 12:15:39 -0400

    Removing no-arg constructor from Command.class

--- src/main/java/bot/modules/commands/octopi/CommandPrinterHistory.java
+++ src/main/java/bot/modules/commands/octopi/CommandPrinterHistory.java
@@ -1,21 +1,19 @@
 package bot.modules.commands.octopi;
 
+import bot.modules.discord.Command;
+import bot.modules.octopi.PrinterEnum;
+import com.google.common.collect.ImmutableList;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.MessageCreateSpec;
+import org.apache.commons.io.FileUtils;
+import reactor.core.publisher.Mono;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
-
-import com.google.common.collect.ImmutableList;
-
-import org.apache.commons.io.FileUtils;
-
-import bot.modules.discord.Command;
-import bot.modules.octopi.PrinterEnum;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.MessageChannel;
-import discord4j.core.spec.MessageCreateSpec;
-import reactor.core.publisher.Mono;
 
 public class CommandPrinterHistory extends Command
 {
@@ -24,41 +22,41 @@ public class CommandPrinterHistory extends Command
 
 	public CommandPrinterHistory(PrinterEnum printerEnum)
 	{
-		this.name = printerEnum.getName() + "-history";
+		super(printerEnum.getName() + "-history");
 		this.url = printerEnum + "/api/printer?history=true&apikey=" + printerEnum.getKey();
 	}
 
     @Override
     public Mono<Message> handle(Message message, MessageChannel channel, ImmutableList<String> strings) {
-		
+
 		final File file;
 		try {
 			//Create temp file
 			file = File.createTempFile("printer_history_" + System.currentTimeMillis(), ".json");
 
 			try(InputStream stream = FileUtils.openInputStream(file)) {
-			
-	
+
+
 				//Copy URL output to file
 				FileUtils.copyURLToFile(new URL(url), file);
-	
+
 				//Post file to channel
 				channel.createMessage(action -> new MessageCreateSpec().addFile(file.getName(), stream)).block();
-	
-				
+
+
 				//Delete file when done
 				Files.delete(file.toPath());
-				
+
 			} catch (Exception e) {
 				channel.createMessage("Error processing command").block();//TODO ping admin
-			} 
+			}
 
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 
-		
+
         return Mono.empty();
     }
 }
--- src/main/java/bot/modules/commands/octopi/CommandPrinterJob.java
+++ src/main/java/bot/modules/commands/octopi/CommandPrinterJob.java
@@ -3,12 +3,9 @@ package bot.modules.commands.octopi;
 import bot.modules.discord.Command;
 import bot.modules.octopi.PrinterEnum;
 import bot.modules.octopi.PrinterUtilities;
+import com.google.common.collect.ImmutableList;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
-import java.util.List;
-
-import com.google.common.collect.ImmutableList;
-
 import reactor.core.publisher.Mono;
 
 public class CommandPrinterJob extends Command {
@@ -16,8 +13,8 @@ public class CommandPrinterJob extends Command {
   private final PrinterEnum printer;
 
   public CommandPrinterJob(PrinterEnum printer) {
+    super(printer.getName() + "-job");
     this.printer = printer;
-    this.name = printer.getName() + "-job";
   }
 
   @Override
