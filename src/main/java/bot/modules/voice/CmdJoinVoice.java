cc14880d321fabaa71bdb15b42e16c1e9319d3b9
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2021-03-31 12:25:17 -0400

    fix joinvoice command

--- src/main/java/bot/modules/voice/CmdJoinVoice.java
+++ src/main/java/bot/modules/voice/CmdJoinVoice.java
@@ -2,6 +2,8 @@ package bot.modules.voice;
 
 import java.util.List;
 
+import com.google.common.collect.ImmutableList;
+
 import bot.modules.discord.Command;
 import discord4j.core.object.VoiceState;
 import discord4j.core.object.entity.Member;
@@ -15,44 +17,29 @@ import reactor.core.publisher.Mono;
 public class CmdJoinVoice extends Command
 {
 
+    VoiceConnection connection;
+    VoiceChannel chan;
+    VoiceState state;
+    Member member;
+
     public CmdJoinVoice(String root) {
         super(root);
     }
 
     @Override
-    public Mono<Message> handle(Message message, MessageChannel channel, List<String> strings) {
-        Member member = message.getAuthor().get().asMember(message.getGuild().block().getId()).block();
-        VoiceState state = member.getVoiceState().block();
-        VoiceChannel chan = state.getChannel().block();
-        AudioReceiver receiver;
-        VoiceConnection connection;
+    public Mono<Message> handle(Message message, MessageChannel channel, ImmutableList<String> strings) {
+         member = message.getAuthor().get().asMember(message.getGuild().block().getId()).block();
+         state = member.getVoiceState().block();
+         chan = state.getChannel().block();
         if(chan.equals(null)){
-            connection = chan.join(spec -> spec.setReceiver(receiver)).block();
+            chan.sendConnectVoiceState(false, false);
+            connection = chan.getVoiceConnection().block();
         }
         return Mono.empty();
     }
-//	private static Logger log = LogManager.getLogger();
-//
-//	public CmdJoinVoice() {
-//		name = "join";
-//		hidden = true;
-//	}
-//
-//	@Override
-//	protected void execute(CommandEvent event) {
-//		AudioManager manager = event.getGuild().getAudioManager();
-//
-//		VoiceChannel chan = event.getGuild().getVoiceChannelById(359460350988517378l);
-//		manager.openAudioConnection(chan);
-//		log.info("connected to channel: {}" + chan.getName());
-//		//manager.setSendingHandler(new AudioSendHandler().);
-//
-//		//Voice voice = Voice.newBuilder().addLanguageCodes("en-US").setSsmlGender(SsmlVoiceGender.FEMALE)
-//		//		.setName("GeekBot").build();
-//		//SynthesisInput input = SynthesisInput.newBuilder().setText("Hello World!").build();
-//		//AudioConfig audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.OGG_OPUS).build();
-//
-//		//TextToSpeechClient ttsClient = TextToSpeechClient.create().synthesizeSpeech(input, voice, audioConfig);
-//	}
+
+    public VoiceConnection getConnection() {
+        return this.connection;
+    }
 
 }
